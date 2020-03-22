530bcf69b10414417e03cfd454af0796eaa52daa
Adil Patel <>
2020-03-22 00:40:35 -0400

    Old version of code

--- ios/Pods/Target Support Files/Pods-bhmhockey-tvOS/Pods-bhmhockey-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bhmhockey_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_bhmhockey_tvOS
-@end
--- ios/Pods/Target Support Files/Pods-bhmhockeyTests/Pods-bhmhockeyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bhmhockeyTests : NSObject
-@end
-@implementation PodsDummy_Pods_bhmhockeyTests
-@end
